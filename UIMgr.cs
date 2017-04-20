4476d676a9ee848a130460f70b1e6350f6ebd0cb
kimseonghwan <kimseonghwan@kimseonghwanui-MacBook-Air.local>
2017-04-20 19:26:41 +0900

    test

--- UIMgr.cs
+++ UIMgr.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UIMgr : MonoBehaviour {
-
+	
 	public void OnClickStartBtn()
 	{
+		Debug.Log ("Click Button");
 		Debug.Log ("Click Button");
 		SceneManager.LoadScene ("scLevel01");
 		SceneManager.LoadScene ("scPlay", LoadSceneMode.Additive);
